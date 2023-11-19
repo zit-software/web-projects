929b78fbec37a6e38e2d4ec1348041c2570a8a10
TranHuy2k2 <giahuytran2002@gmail.com>
2023-11-19 17:34:01 +0700

    chore: data for web1

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -21,29 +21,16 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#!">Home</a>
-          </li>
-          <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              id="navbarDropdown"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-              >Shop</a
-            >
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#!">All Products</a></li>
-              <li>
-                <hr class="dropdown-divider" />
-              </li>
-              <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-              <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-            </ul>
-          </li>
+          <RouterLink to="/">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page">Home</a>
+            </li>
+          </RouterLink>
+          <RouterLink to="/customer">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page">Hồ sơ</a>
+            </li>
+          </RouterLink>
         </ul>
         <div class="d-flex gap-1">
           <RouterLink class="btn btn-outline-dark" to="/cart">
